34fafebfb33431dcf15261678c3a899782c0a5f0
KwanHo <kh7939@gmail.com>
2025-08-26 00:51:16 -0500

    update

--- 16_function.c
+++ 16_function.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /*
-Thing	Goes to caller?	Shown on screen?
+Thing	Goes to caller?  	Shown on screen?
 return		✅ yes			❌ no (unless you print it)
 printf		❌ no			✅ yes
 */
 
+// Using "()", to call a function
+
 void with_out_return();
 int with_return();
-
+void with_para();
+int with_para_return();
 
 int main() {
+	srand(time(NULL));
 
 	with_out_return();
 	
+	//wiil do anything except returning value
 	with_return();
-
+	
+	//will go thru the function and return value
 	int result = with_return();
 	printf("%d\n", result);
 	
+	//will also go thru the function and return value
 	printf("%d\n", with_return());
+
+	with_para(1, 3, 5);
+
+	int num1 = rand();
+	int num2 = rand();
+	printf("result : %d\n" ,with_para_return(num1, num2));
+	printf("random max : %d\n", RAND_MAX);
 	return 0;
 }
 
@@ -28,6 +43,14 @@ void with_out_return() {
 }
 
 int with_return() {
-	printf("hehe\n");
+	printf("haha\n");
 	return 10;
+}
+
+void with_para(int num1, int num2, int num3) {
+	printf("%d, %d, %d\n", num1, num2, num3);
+}
+
+int with_para_return(int max, int min) {
+	return max-min;
 }
